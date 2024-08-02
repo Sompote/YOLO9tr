2cd3c1c19a9f8c9f9c5b683af9e9b141623737fe
Sompote <62241733+Sompote@users.noreply.github.com>
2024-08-02 11:44:01 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,44 @@
-
-import subprocess
-
 import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import subprocess
 import os
 from PIL import Image
 import torch
 import sys
-#import cv2
 
+# Assuming detect_dual.py has a run_detection function
+from detect_dual import run as yolo_run_detection
+
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 new_env = os.environ.copy()
+
 def add_logo(logo_path, size=(200, 150)):
-    logo = Image.open('logoAI.png')
+    logo = Image.open(logo_path)
     logo = logo.resize(size)
     st.image(logo, use_column_width=False)
 
 def run_detection(image_path):
-    env = os.environ.copy()
-    env['PYTHONPATH'] = '/mount/src/yolo9tr/' 
-    
-    # Run the detection command
-    command = [
-        "python", "detect_dual.py",
-        "--source", image_path,
-        "--img", "640",
-        "--device", "cpu",
-        "--weights", "models/detect/yolov9tr.pt",
-        "--name", "yolov9_c_640_detect",
-        "--exist-ok"
-    ]
-    subprocess.run(command, check=True, env=new_env)
-
-    # Find the output image
+    # Directly call the function from detect_dual.py
+    yolo_run_detection(
+        source=image_path,
+        imgsz=640,
+        device="cpu",
+        weights="models/detect/yolov9tr.pt",
+        name="yolov9_c_640_detect",
+        exist_ok=False)
+    #Find the output image
     output_dir = "runs/detect/yolov9_c_640_detect"
     output_image = os.path.join(output_dir, os.path.basename(image_path))
     return output_image
+    
+    
 
 def main():
     st.title("YOLO9tr Object Detection")
 
     # Add the research center logo at the top of the app
-    add_logo("research_center_logo.png")
+    add_logo("logo_ai.jpg")
 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
